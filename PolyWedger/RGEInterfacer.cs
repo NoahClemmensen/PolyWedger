fd46355b363d69a3596f2c7de6260b17daa50da7
CodingWithBoris <boris60601@gmail.com>
2025-11-27 21:31:12 +0100

    Here you go bjom gl Broken

---
+++ PolyWedger/RGEInterfacer.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace PolyWedger
+{
+    public class RGEInterfacer
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct INPUT
+        {
+            public uint type;
+            public InputUnion U;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct InputUnion
+        {
+            [FieldOffset(0)] public MOUSEINPUT mi;
+            [FieldOffset(0)] public KEYBDINPUT ki;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx, dy;
+            public uint mouseData, dwFlags, time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public ushort wVk, wScan;
+            public uint dwFlags, time;
+            public IntPtr dwExtraInfo;
+        }
+
+        const uint INPUT_MOUSE = 0;
+        const uint INPUT_KEYBOARD = 1;
+        const uint MOUSEEVENTF_LEFTDOWN = 0x02;
+        const uint MOUSEEVENTF_LEFTUP = 0x04;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+
+        [DllImport("user32.dll")] static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+        [DllImport("user32.dll")] static extern bool SetCursorPos(int x, int y);
+        [DllImport("user32.dll")] static extern bool OpenClipboard(IntPtr hwnd);
+        [DllImport("user32.dll")] static extern bool CloseClipboard();
+        [DllImport("user32.dll")] static extern bool EmptyClipboard();
+        [DllImport("user32.dll")] static extern bool SetClipboardData(uint uFormat, IntPtr data);
+        const uint CF_UNICODETEXT = 13;
+
+        void PressKey(ushort vk)
+        {
+            INPUT[] inp = new INPUT[2];
+
+            inp[0].type = INPUT_KEYBOARD;
+            inp[0].U.ki.wVk = vk;
+
+            inp[1].type = INPUT_KEYBOARD;
+            inp[1].U.ki.wVk = vk;
+            inp[1].U.ki.dwFlags = KEYEVENTF_KEYUP;
+
+            SendInput(2, inp, Marshal.SizeOf(typeof(INPUT)));
+            Thread.Sleep(12);
+        }
+
+        void PressCombo(ushort mod, ushort key)
+        {
+            PressKey(mod);
+            PressKey(key);
+
+            INPUT up = new INPUT { type = INPUT_KEYBOARD };
+            up.U.ki.wVk = mod;
+            up.U.ki.dwFlags = KEYEVENTF_KEYUP;
+            SendInput(1, new INPUT[] { up }, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        void RobloxClick(int x, int y, int cycles = 2)
+        {
+            SetCursorPos(x, y);
+            Thread.Sleep(5);
+
+            int[,] offsets = new int[,]
+            {
+                { 0,0 }, { 5,0 }, { 5,5 }, { 0,5 }, { -5,5 },
+                { -5,0 }, { -5,-5 }, { 0,-5 }, { 5,-5 }
+            };
+
+            for (int c = 0; c < cycles; c++)
+            {
+                for (int i = 0; i < offsets.GetLength(0); i++)
+                {
+                    int ox = x + offsets[i, 0];
+                    int oy = y + offsets[i, 1];
+
+                    SetCursorPos(ox, oy);
+                    Thread.Sleep(4);
+
+                    INPUT[] click = new INPUT[2];
+
+                    click[0].type = INPUT_MOUSE;
+                    click[0].U.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+                    click[1].type = INPUT_MOUSE;
+                    click[1].U.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+                    SendInput(2, click, Marshal.SizeOf(typeof(INPUT)));
+                }
+            }
+        }
+
+        void CopyUID()
+        {
+            PressCombo(0x11, 0x43);
+        }
+
+        void PasteUID()
+        {
+            PressCombo(0x11, 0x56);
+        }
+
+        public void RunTriangleWorkflow()
+        {
+            int spawnX = 100, spawnY = 200;
+            int centerX = 960, centerY = 540;
+            int uidX = 300, uidY = 300;
+            int cmdX = 200, cmdY = 1000;
+
+            RobloxClick(spawnX, spawnY);
+
+            Thread.Sleep(200);
+
+            RobloxClick(centerX, centerY);
+
+            Thread.Sleep(200);
+
+            RobloxClick(uidX, uidY);
+
+            CopyUID();
+
+            Thread.Sleep(150);
+
+            RobloxClick(cmdX, cmdY);
+
+            Thread.Sleep(150);
+
+            PasteUID();
+            PressKey(0x0D);
+        }
+    }
+}
--- RGEInterfacerTest/Program.cs
+++ RGEInterfacerTest/Program.cs
@@ -1,9 +1,210 @@
-﻿namespace RGEInterfacerTest;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Windows.UI.Input.Preview.Injection;
 
-class Program
+namespace PolyWedger
 {
-    static void Main(string[] args)
+    public sealed class RawInputHidSender
     {
-        Console.WriteLine("Hello, World!");
+        private const int AbsoluteRange = 65535;
+
+        private readonly InputInjector _injector;
+        private readonly int _originX;
+        private readonly int _originY;
+        private readonly int _width;
+        private readonly int _height;
+
+        private const int SM_XVIRTUALSCREEN = 76;
+        private const int SM_YVIRTUALSCREEN = 77;
+        private const int SM_CXVIRTUALSCREEN = 78;
+        private const int SM_CYVIRTUALSCREEN = 79;
+
+        public RawInputHidSender()
+        {
+            _injector = InputInjector.TryCreate() ?? throw new InvalidOperationException("InputInjector is unavailable. Enable HID input injection (Win10+) or install the HID injection driver.");
+
+            _originX = GetSystemMetrics(SM_XVIRTUALSCREEN);
+            _originY = GetSystemMetrics(SM_YVIRTUALSCREEN);
+            _width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+            _height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+
+            if (_width <= 0 || _height <= 0)
+            {
+                _width = GetSystemMetrics(0);
+                _height = GetSystemMetrics(1);
+            }
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            var mouseMove = new InjectedInputMouseInfo
+            {
+                MouseOptions = InjectedInputMouseOptions.Move | InjectedInputMouseOptions.Absolute | InjectedInputMouseOptions.VirtualDesk,
+                DeltaX = NormalizeToAbsolute(x, _originX, _width),
+                DeltaY = NormalizeToAbsolute(y, _originY, _height)
+            };
+
+            _injector.InjectMouseInput(new[] { mouseMove });
+        }
+
+        public void LeftClickAt(int x, int y)
+        {
+            int nx = NormalizeToAbsolute(x, _originX, _width);
+            int ny = NormalizeToAbsolute(y, _originY, _height);
+
+            _injector.InjectMouseInput(new[]
+            {
+                new InjectedInputMouseInfo
+                {
+                    MouseOptions = InjectedInputMouseOptions.Move | InjectedInputMouseOptions.Absolute | InjectedInputMouseOptions.VirtualDesk,
+                    DeltaX = nx,
+                    DeltaY = ny
+                },
+                new InjectedInputMouseInfo
+                {
+                    MouseOptions = InjectedInputMouseOptions.LeftDown | InjectedInputMouseOptions.Absolute | InjectedInputMouseOptions.VirtualDesk,
+                    DeltaX = nx,
+                    DeltaY = ny
+                },
+                new InjectedInputMouseInfo
+                {
+                    MouseOptions = InjectedInputMouseOptions.LeftUp | InjectedInputMouseOptions.Absolute | InjectedInputMouseOptions.VirtualDesk,
+                    DeltaX = nx,
+                    DeltaY = ny
+                }
+            });
+        }
+
+        public void PressKey(ushort vk)
+        {
+            _injector.InjectKeyboardInput(new[]
+            {
+                new InjectedInputKeyboardInfo { VirtualKey = vk, KeyOptions = InjectedInputKeyOptions.None },
+                new InjectedInputKeyboardInfo { VirtualKey = vk, KeyOptions = InjectedInputKeyOptions.KeyUp }
+            });
+            Thread.Sleep(12);
+        }
+
+        public void PressCombo(ushort mod, ushort key)
+        {
+            _injector.InjectKeyboardInput(new[]
+            {
+                new InjectedInputKeyboardInfo { VirtualKey = mod, KeyOptions = InjectedInputKeyOptions.None },
+                new InjectedInputKeyboardInfo { VirtualKey = key, KeyOptions = InjectedInputKeyOptions.None },
+                new InjectedInputKeyboardInfo { VirtualKey = key, KeyOptions = InjectedInputKeyOptions.KeyUp },
+                new InjectedInputKeyboardInfo { VirtualKey = mod, KeyOptions = InjectedInputKeyOptions.KeyUp }
+            });
+            Thread.Sleep(12);
+        }
+
+        private static int NormalizeToAbsolute(int coordinate, int origin, int span)
+        {
+            if (span <= 1)
+            {
+                return 0;
+            }
+
+            int clamped = Math.Clamp(coordinate, origin, origin + span - 1);
+            double relative = (clamped - origin) * (double)AbsoluteRange / (span - 1);
+            return (int)Math.Round(relative);
+        }
+        
+        [DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(int nIndex);
+    }
+
+    public class RGEInterfacer
+    {
+        private readonly RawInputHidSender _hid;
+        private readonly Random _rng = new Random();
+
+        public RGEInterfacer(RawInputHidSender hid)
+        {
+            _hid = hid;
+        }
+
+        void RobloxClick(int x, int y, int jitterMoves = 3)
+        {
+            int moves = Math.Max(0, jitterMoves - 1);
+
+            for (int i = 0; i < moves; i++)
+            {
+                int jitterX = x + _rng.Next(-2, 3);
+                int jitterY = y + _rng.Next(-2, 3);
+
+                _hid.MoveTo(jitterX, jitterY);
+                Thread.Sleep(10);
+            }
+
+            _hid.MoveTo(x, y);
+            Thread.Sleep(100);
+            _hid.LeftClickAt(x, y);
+            Thread.Sleep(100);
+        }
+
+        void CopyUIDFromField(int x, int y)
+        {
+            RobloxClick(x, y, 1); 
+            RobloxClick(x, y, 1);
+            Thread.Sleep(1000);
+            _hid.PressCombo(0x11, 0x43);
+            Thread.Sleep(1000);
+        }
+
+        void PasteUID()
+        {
+            _hid.PressCombo(0x11, 0x56);
+        }
+
+        public void RunTriangleWorkflow()
+        {
+            int spawnX = 840, spawnY = 65;
+            int centerX = 996, centerY = 475;
+            int cmdX = 914, cmdY = 1033;
+            int uidX = 1827, uidY = 227;
+
+            RobloxClick(spawnX, spawnY);
+
+            Thread.Sleep(200);
+
+            RobloxClick(centerX, centerY);
+
+            Thread.Sleep(200);
+
+            RobloxClick(uidX, uidY);
+            CopyUIDFromField(uidX, uidY);
+
+            Thread.Sleep(300);
+
+            RobloxClick(cmdX, cmdY);
+
+            Thread.Sleep(150);
+
+            PasteUID();
+            _hid.PressKey(0x0D);
+        }
+    }
+}
+
+namespace PolyWedger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RGEInterfacer HID Test Runner");
+            Console.WriteLine("You have 3 seconds to focus Roblox...");
+            Thread.Sleep(3000);
+
+            RawInputHidSender hid = new RawInputHidSender();
+            RGEInterfacer rge = new RGEInterfacer(hid);
+
+            Console.WriteLine("Running triangle workflow via HID injection...");
+            rge.RunTriangleWorkflow();
+
+            Console.WriteLine("Done! Press any key to exit.");
+            Console.ReadKey();
+        }
     }
 }
